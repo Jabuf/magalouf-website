97e286619b4b462f704f666afe3def790fafeb9f
Jabuf <jason.buffet@gmail.com>
2023-05-29 17:19:24 +0200

    add survey component

---
+++ src/data/pollTdm.js
@@ -0,0 +1,77 @@
+export const pollTdmJson = {
+   'elements': [
+    {
+     'type': 'dropdown',
+     'name': 'jaune',
+     'title': 'Maillot jaune',
+     'isRequired': true,
+     'choices': [
+      {
+       'value': 'Item 1',
+       'text': 'Jacquouille'
+      },
+      {
+       'value': 'Item 2',
+       'text': 'Bébert'
+      },
+      {
+       'value': 'Item 3',
+       'text': 'Blo-is'
+      },
+      {
+       'value': 'Item 4',
+       'text': 'Valde'
+      }
+     ]
+    },    {
+     'type': 'dropdown',
+     'name': 'vert',
+     'title': 'Maillot vert',
+     'isRequired': true,
+     'choices': [
+      {
+       'value': 'Item 1',
+       'text': 'Jacquouille'
+      },
+      {
+       'value': 'Item 2',
+       'text': 'Bébert'
+      },
+      {
+       'value': 'Item 3',
+       'text': 'Blo-is'
+      },
+      {
+       'value': 'Item 4',
+       'text': 'Valde'
+      }
+     ]
+    },
+    {
+     'type': 'dropdown',
+     'name': 'pois',
+     'title': 'Maillot à pois',
+     'isRequired': true,
+     'choices': [
+      {
+       'value': 'Item 1',
+       'text': 'Jacquouille'
+      },
+      {
+       'value': 'Item 2',
+       'text': 'Bébert'
+      },
+      {
+       'value': 'Item 3',
+       'text': 'Blo-is'
+      },
+      {
+       'value': 'Item 4',
+       'text': 'Valde'
+      }
+     ]
+    }
+
+   ],
+   'title': 'Journée du 17 juin'
+}
--- src/pages/TourDeMagaluf.vue
+++ src/pages/TourDeMagaluf.vue
@@ -1,3 +1,19 @@
 <template>
-SOON
+  <div id="survey" />
 </template>
+
+<script>
+import 'survey-core/defaultV2.min.css'
+import { StylesManager } from 'survey-core'
+import {Survey} from 'survey-knockout-ui'
+import { pollTdmJson } from '@/data/pollTdm.js'
+
+StylesManager.applyTheme('defaultV2')
+
+export default {
+  mounted() {
+    const survey = new Survey(pollTdmJson)
+    survey.render('survey')
+  }
+}
+</script>
